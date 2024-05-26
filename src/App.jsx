f59e13912f5d83bc9d32004d26c7b4c3cc1f708a
Antonio-pagliacci <contato.antoniopagliacci@gmail.com>
2024-05-26 12:13:00 -0300

    Projeto sistema de loguin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Loguin from './components/Loguin'
+
+function App() {
+  
+  return <>
+      <div className="App">
+      <Loguin/>
+
+      </div>
+    </>
+}
+
+export default App
